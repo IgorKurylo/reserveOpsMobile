35993855de1961f94c6c4795a52184500afba12c
Igork <igork@korentec.co.il>
2021-04-01 23:06:47 +0300

    adding register layout, finish login layout

--- app/src/main/java/com/ops/ui/LoginActivity.java
+++ app/src/main/java/com/ops/ui/LoginActivity.java
@@ -3,13 +3,16 @@ package com.ops.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.ops.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
+    TextView registerTxtView;
     EditText phoneNumberEdTxt;
     Button logInBtn;
 
@@ -19,6 +22,14 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         phoneNumberEdTxt = findViewById(R.id.phoneNumberEditText);
         logInBtn = findViewById(R.id.loginBtn);
+        registerTxtView = findViewById(R.id.registerTxtView);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.registerTxtView) {
+
+        }
     }
 }
